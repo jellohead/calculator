ce2905def11d36d27134df2ff4cea744c93da1d7
Pat Escalona <pat.escalona7@gmail.com>
2022-09-28 11:44:44 -0500

    Keypad buttons are working

--- calculator.js
+++ calculator.js
@@ -7,7 +7,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = function (a, b) {
     console.log("divide function a = " + a + ", b = " + b);
-
+    //need handler for 0/0, result is NaN
     if (Number.isFinite(a / b)) {
         return a / b;
     } else {
@@ -38,32 +38,11 @@ const operate = (a, b, operator) => {
 const push = (item) => numberStack.push(item);
 const pop = () => numberStack.pop();
 
-
-//this needs to be wrapped in some sort of init loop
 const buttons = document.querySelectorAll('button');
 console.log(buttons);
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e);
+        console.log(e.target.id);
         //take action for button clicked
     })
 })
-
-
-
-// let a = prompt("enter a number");
-// console.log("a = " + a);
-// let b = prompt("enter another number");
-// console.log("b = " + b);
-// let operator = prompt("enter +, -, * or /");
-// console.log("operator = " + operator);
-// console.log("The answer is " + operate(a, b, operator));
-
-
-// // Do not edit below this line
-// module.exports = {
-//     add,
-//     subtract,
-//     multiply,
-//     divide
-// };
