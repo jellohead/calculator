6166a132b32010deea99cae9f09024050209de29
Pat Escalona <pat.escalona7@gmail.com>
2022-09-28 11:53:13 -0500

    merged and cleaned up some errors

--- calculator.js
+++ calculator.js
@@ -35,24 +35,8 @@ const operate = (a, b, operator) => {
     }
 }
 
-<<<<<<< HEAD
-// let a = prompt("enter a number");
-// console.log("a = " + a);
-// let b = prompt("enter another number");
-// console.log("b = " + b);
-// let operator = prompt("enter +, -, * or /");
-// console.log("operator = " + operator);
-// console.log("The answer is " + operate(a, b, operator));
-
-// equal buttons calls operate function
-// set up event listeners for each button
-// save entered numbers and operator in variables
-// should i use two stacks to hold numbers and operators?
-
-=======
 const push = (item) => numberStack.push(item);
 const pop = () => numberStack.pop();
->>>>>>> keypad
 
 const buttons = document.querySelectorAll('button');
 console.log(buttons);
