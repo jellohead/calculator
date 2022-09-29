3ce1e764e7c04fb329cdaeb7d59338ae41c3b3ab
pat <pat.escalona7@gmail.com>
2022-09-29 09:56:28 -0500

    Close enough to done for message
    stack ops still need some tweaking
    exception handing still needs work
    could try to make it prettier

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ let numberStack = [],
 
 const displayDiv = document.querySelector('.display');
 
-const add = (a, b) => +a + +b;
+const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = function (a, b) {
@@ -33,7 +33,7 @@ const clear = () => {
 const operate = (a, b, operator) => {
     switch (operator) {
         case "+":
-            displayDiv.textContent = add(a, b);
+            return add(a, b);
         case "-":
             return subtract(a, b);
         case "*":
@@ -42,7 +42,9 @@ const operate = (a, b, operator) => {
             return divide(a, b);
         case "=":
             // executeOp();
-            operate(executeOp());
+            console.log("operate: in equal case");
+            console.log("a = " + a + ", b = " + b + ", operator is " + operator);
+            operate(a, b, operate);
         case "clear":
             return clear();
     }
@@ -54,25 +56,37 @@ const keypadEntry = function (id, text) {
         id === "plus" ||
         id === "minus" ||
         id === "times" ||
-        id === "divide" ||
-        id === "equal") {
+        id === "divide"
+    ) {
         pushNumber(displayDiv.textContent);
         displayDiv.textContent = "";
         pushOperator(text);
         console.log("keypad: entered an operator");
+    } else if (id === "equal") {
+        b = popNumber();
+        console.log("keypad: b popNumber is " + b);
+        a = popNumber();
+        console.log("keypad: a popNumber is " + a);
+        operator = popOperator();
+        console.log("keypad: operator popOperator is " + operator);
+        console.log("keypad: text is " + text);
+        let result = operate(a, b, operator);
+        console.log("keypad: result is " + result);
+        displayDiv.textContent = result;
     } else {
         display(id, text);
+        pushNumber(text);
     }
 }
 
 const pushNumber = (item) => {
-    numberStack.push(item);
-    console.log("pushNumber pushed item = " + item);
+    numberStack.push(Number(item));
+    console.log("pushNumber pushed item " + item);
 }
 
 const pushOperator = (item) => {
     operatorStack.push(item);
-    console.log("pushOperator pushed item = " + item);
+    console.log("pushOperator pushed item " + item);
 }
 
 const popNumber = () => numberStack.pop();
@@ -84,8 +98,8 @@ console.log(buttons);
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         console.log("buttons.forEach: e.target.id " + e.target.id);
-        console.log("button: record = " + e);
-        console.log("button: text = " + e.target.firstChild.data);
+        console.log("button: record is " + e);
+        console.log("button: text is  " + e.target.firstChild.data);
         keypadEntry(e.target.id, e.target.firstChild.data);
         //take action for button clicked
     })
